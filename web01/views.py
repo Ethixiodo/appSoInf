6e41d2687925999fa5524f6ffbef75bb331dfb73
Marcelo Ivan Soto Ojeda <ing.marcelosoto@gmail.com>
2023-03-17 16:36:23 -0300

    Se agrego campo home.html con redireccion en views y settings mayormente afectado

--- web01/views.py
+++ web01/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# import string
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'web01/home.html', {'name': 'Ethi'})
